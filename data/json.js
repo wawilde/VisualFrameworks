9f0888a0f2e24f8640695ce49047da8f34c21d11
Woody Wilde <wawilde@fullsail.edu>
2013-01-16 13:13:12 -0500

    added items to css and JSON data
    also added to main.js

---
+++ data/json.js
@@ -0,0 +1,12 @@
+//JSON DATA
+//WOODY WILDE
+//VISUAL FRAMEWORKS
+//0113
+//PROJECT 1
+
+{
+    "Quote": "Four score and seven years ago...",
+    "Type": "Famous",
+    "author": "Abraham Lincoln" ,
+    "date": " ",
+}
---
+++ js/main.js
@@ -0,0 +1,8 @@
+/*Woody Wilde
+ *visual frameworks
+ *0113
+ *project1
+ */
+ 
+console.log("this is working now make something out of it");
+ 
